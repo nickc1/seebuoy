8818de0b2ff8cb3c5b88d3f4f8355e872f2ce9ca
nickc1 <nickcortale@gmail.com>
2020-05-02 14:41:40 -0400

    started refactor. Not currently working

--- seebuoy/ndbc/real_time.py
+++ seebuoy/ndbc/real_time.py
@@ -58,6 +58,115 @@ import pandas as pd
 import numpy as np
 import datetime
 
+
+def real_time(buoy, dataset='txt'):
+    """Get realtime data from the NDBC. They have six different data sources
+    to pull from:
+
+    Parameters
+    ----------
+    buoy : int
+        Integer of the buoy identifier. Look up buoy numbers here:
+        https://www.ndbc.noaa.gov/
+    source: str
+        What type of data to query. Possible values are:
+        - data_spec: Raw Spectral Wave Data
+        - ocean: Oceanographic Data
+        - spec: Spectral Wave Summary Data
+        - supl: Supplemental Measurements Data
+        - swdir: Spectral Wave Data (alpha1)
+        - swdir2: Spectral Wave Data (alpha2)
+        - swr1: Spectral Wave Data (r1)
+        - swr2: Spectral Wave Data (r2)
+        - txt: Standard Meteorological Data
+
+    Returns
+    -------
+    df : pd.DataFrame
+        DataFrame containing the requested data.
+    
+    Notes
+    -----
+
+    The realtime data for all of their buoys can be found at:
+    http://www.ndbc.noaa.gov/data/realtime2/
+
+    Info about all of noaa data can be found at:
+    http://www.ndbc.noaa.gov/docs/ndbc_web_data_guide.pdf
+
+    What all the values mean:
+    http://www.ndbc.noaa.gov/measdes.shtml
+
+
+    Example:
+    from seebuoy import ndbc
+
+    # Get the last 45 days of data
+    df = ndbc.real_time(41013)
+    df.head()
+
+    Out[7]:
+                        WVHT    SwH SwP WWH WWP SwD WWD STEEPNESS   APD MWD
+    2016-02-04 17:42:00 1.6     1.3 7.1 0.9 4.5 S   S   STEEP       5.3 169
+    2016-02-04 16:42:00 1.7     1.5 7.7 0.9 5.0 S   S   STEEP       5.4 174
+    2016-02-04 15:41:00 2.0     0.0 NaN 2.0 7.1 NaN S   STEEP       5.3 174
+    2016-02-04 14:41:00 2.0     1.2 7.7 1.5 5.9 SSE SSE STEEP       5.5 167
+    2016-02-04 13:41:00 2.0     1.7 7.1 0.9 4.8 S   SSE STEEP       5.7 175
+    """
+    df = pd.DataFrame()
+
+    return df
+
+
+def _make_url(buoy, dataset):
+    """Create the link to the requested dataset
+    buoy: int
+    dataset: str
+    """"
+    url = 'http://www.ndbc.noaa.gov/data/realtime2/{}.{}'
+    return url.format(buoy, dataset)
+
+
+def _data_spec(buoy):
+    """
+    Get the raw spectral wave data from the buoy. The seperation
+    frequency is dropped to keep the data clean.
+
+    Parameters
+    ----------
+    buoy : str
+        Buoy number ex: '41013' is off wilmington, nc
+
+    Returns
+    -------
+    df : pd.DataFrame (date, frequency)
+        data frame containing the raw spectral data. index is the date
+        and the columns are each of the frequencies
+
+    """
+    
+    url = _make_url(buoy, dataset)
+
+    #combine the first five date columns YY MM DD hh mm and make index
+    df = pd.read_csv(link, delim_whitespace=True, skiprows=1, header=None,
+        parse_dates=[[0,1,2,3,4]], index_col=0)
+
+
+    #convert the dates to datetimes
+    df.index = pd.to_datetime(df.index,format="%Y %m %d %H %M")
+
+    specs = df.iloc[:,1::2]
+    freqs = df.iloc[0,2::2]
+
+    specs.columns=freqs
+
+    #remove the parenthesis from the column index
+    specs.columns = [cname.replace('(','').replace(')','')
+        for cname in specs.columns]
+
+    return specs
+
+
 class RealTime:
 
     def __init__(self, buoy):
@@ -324,7 +433,7 @@ class RealTime:
         link = "{}.{}".format(self.link, 'swr1')
         #combine the first five date columns YY MM DD hh mm and make index
         df = pd.read_csv(link,delim_whitespace=True,skiprows=1,
-            header=None, parse_dates=[[0,1,2,3,4]], index_col=0)
+            header=None, parse_dates=[[0,1,2,3,4]], index_col=0, na_values=999)
 
         #convert the dates to datetimes
         df.index = pd.to_datetime(df.index,format="%Y %m %d %H %M")
