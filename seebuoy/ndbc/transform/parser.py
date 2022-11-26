220c27c43fa523020613758d9b1d153f491f227b
nick cortale <nickcortale@gmail.com>
2022-11-26 10:35:27 -0500

    all parsers

--- seebuoy/ndbc/transform/parser.py
+++ seebuoy/ndbc/transform/parser.py
@@ -23,3 +23,68 @@ def oceanographic(txt, data_group, rename_cols=True):
         df = historic.oceanographic(txt, rename_cols=rename_cols)
     
     return df
+
+def supplemental(txt, data_group, rename_cols=True):
+
+    if data_group == "recent":
+        df = recent.supplemental(txt, rename_cols=rename_cols)
+    
+    else:
+        df = historic.supplemental(txt, rename_cols=rename_cols)
+    
+    return df
+
+
+def raw_spectral(txt, data_group, rename_cols=True):
+
+    if data_group == "recent":
+        df = recent.raw_spectral(txt, rename_cols=rename_cols)
+    
+    else:
+        df = historic.raw_spectral(txt, rename_cols=rename_cols)
+    
+    return df
+
+
+def spectral_alpha1(txt, data_group, rename_cols=True):
+
+    if data_group == "recent":
+        df = recent.spectral_alpha1(txt, rename_cols=rename_cols)
+    
+    else:
+        df = historic.spectral_alpha1(txt, rename_cols=rename_cols)
+    
+    return df
+
+
+def spectral_alpha2(txt, data_group, rename_cols=True):
+
+    if data_group == "recent":
+        df = recent.spectral_alpha2(txt, rename_cols=rename_cols)
+    
+    else:
+        df = historic.spectral_alpha2(txt, rename_cols=rename_cols)
+    
+    return df
+
+
+def spectral_r1(txt, data_group, rename_cols=True):
+
+    if data_group == "recent":
+        df = recent.spectral_r1(txt, rename_cols=rename_cols)
+    
+    else:
+        df = historic.spectral_r1(txt, rename_cols=rename_cols)
+    
+    return df
+
+
+def spectral_r2(txt, data_group, rename_cols=True):
+
+    if data_group == "recent":
+        df = recent.spectral_r2(txt, rename_cols=rename_cols)
+    
+    else:
+        df = historic.spectral_r2(txt, rename_cols=rename_cols)
+    
+    return df
